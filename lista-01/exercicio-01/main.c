34a5cc29952bcb63badbc09110c3c7a00c6cd839
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-18 13:25:26 -0300

    Update main.c

--- lista-01/exercicio-01/main.c
+++ lista-01/exercicio-01/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include "lib.h"
 
+int main(int argc, char *argv[]) {
+	
+	int quantidade, i;
+	
+	printf("Informe a quantidade de alunos: ");
+	scanf("%d", &quantidade);
+	
+	float notas[quantidade];
+	
+	for (i=0; i<quantidade; i++) {
+		printf("Informe a nota do aluno %d: ", i+1);
+		scanf("%f", &notas[i]);
+	}	
+	
+	printf("Vetor original: ");
+	imprimir(notas, quantidade, 0);	
+	
+	printf("Após ordenar(): ");
+	ordenar(notas, quantidade);	
+	imprimir(notas, quantidade, 0);
+	
+	printf("Após embaralhar(): ");
+	embaralhar(notas, quantidade);	
+	imprimir(notas, quantidade, 0);
+	
+	printf("Após ordenar_rec(): ");
+	ordenar_rec(notas, quantidade);
+	imprimir(notas, quantidade, -1);
+	
+	return 0;
+}
