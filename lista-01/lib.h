2aae15a36811fcabddbc7c112a9a78d8fbeb6eda
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-18 13:26:19 -0300

    Create lib.h

---
+++ lista-01/lib.h
@@ -0,0 +1,12 @@
+#ifndef LIB
+#define LIB
+
+void imprimir(float vetor[], int n, int ordem);
+
+void embaralhar(float vetor[], int n);
+
+void ordenar(float vetor[], int n);
+
+void ordenar_rec(float vetor[], int n);
+
+#endif
