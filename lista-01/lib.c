e07c8ef1ae614218da97e59ffb068cbce6fccd1f
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-19 11:02:07 -0300

    Update lib.c

--- lista-01/lib.c
+++ lista-01/lib.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+// se o parâmetro ordem for negativo, imprime na ordem inversa!
 void imprimir(float vetor[], int n, int ordem){
 	if (ordem >= 0) {
 		for (int i=0; i<n; i++) {
@@ -16,40 +18,52 @@ void imprimir(float vetor[], int n, int ordem){
   
 void embaralhar(float vetor[], int n) {
 	srand(time(NULL));
-	for (i = n-1; i>0; i--) {
-        	int j = rand() % (i + 1);
-        	float temp = vetor[i];
-        	vetor[i] = vetor[j];
-        	vetor[j] = temp;
+	for (int i=n-1; i>0; i--) {
+		int j = rand() % (i + 1);
+		float temp = vetor[i];
+		vetor[i] = vetor[j];
+		vetor[j] = temp;
 	}
 }
 
-void ordenar(float vetor[], int n) {
-	int swapped;    
-	for (i=0; i<n-1; i++) {
-		swapped = 0;
-        	for (j=0; j<n-i-1; j++) {
+void ordenar(float vetor[], int n) {  
+	for (int i=0; i<n-1; i++) {
+		int swapped = 0;
+		for (int j=0; j<n-i-1; j++) {
 			if (vetor[j] > vetor[j + 1]) {
-                		float temp = vetor[j];
-                		vetor[j] = vetor[j + 1];
-                		vetor[j + 1] = temp;
-                		swapped = 1;
-            		}
-        	}
-	        if (swapped == 0) {
-	        	break;
-	        }
-    	}
+				float temp = vetor[j];
+				vetor[j] = vetor[j + 1];
+				vetor[j + 1] = temp;
+				swapped = 1;
+			}
+		}
+		if (swapped == 0) break;
+	}
 }
+
+
+/* Poderia ser feito sem a otimização com o swapped
+void ordenar(float vetor[], int n) {  
+	for (int i=0; i<n-1; i++) {
+		for (int j=0; j<n-i-1; j++) {
+			if (vetor[j] > vetor[j + 1]) {
+				float temp = vetor[j];
+				vetor[j] = vetor[j + 1];
+				vetor[j + 1] = temp;
+			}
+		}
+	}
+} */
+
 
 void ordenar_rec(float vetor[], int n){ 
 	if (n < 1) return;  	
 	for (int i=0; i<n-1; i++) {
-        	if (vetor[i] > vetor[i+1]) {
-        		float temp = vetor[i];
-        		vetor[i] = vetor[i+1];
-        		vetor[i+1] = temp;
+		if (vetor[i] > vetor[i+1]) {
+			float temp = vetor[i];
+			vetor[i] = vetor[i+1];
+			vetor[i+1] = temp;
 		}
 	}
-    	ordenar_rec(vetor, n-1); 
+	ordenar_rec(vetor, n-1); 
 }
