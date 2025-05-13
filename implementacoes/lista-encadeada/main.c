e311a9bca3c30ea3123218e621fab4c4252c15b6
cairuga <62620754+cairuga@users.noreply.github.com>
2025-05-13 15:42:12 -0300

    Create main.c

---
+++ implementacoes/lista-encadeada/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lista-encadeada.h"
+
+int main() {
+   Lista* lista = criar_lista();
+   inserir_ordem(20, lista);
+   inserir_ordem(33, lista);
+   inserir_ordem(19, lista);
+   imprimir(lista);           // Lista: 19 20 33 
+   remover_inicio(lista);
+   remover_fim(lista);
+   inserir_fim(40, lista);
+   inserir_fim(50, lista);
+   inserir_ordem(45, lista);
+   printf("Tamanho: %d\n", tamanho(lista)); // 4
+   remover_valor(40, lista);
+   imprimir(lista);           // Lista: 20 45 50
+   liberar_lista(lista);
+   return 0;
+}
