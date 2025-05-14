dd0504fd39aa0b8fac6aa9a0575eb39d78d42833
cairuga <62620754+cairuga@users.noreply.github.com>
2025-05-14 13:07:36 -0300

    Create lista_dup_encadeada.h

---
+++ implementacoes/lista-dup-encadeada/lista_dup_encadeada.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_DUP_ENCADEADA_H
+#define LISTA_DUP_ENCADEADA_H
+
+// Estrutura do elemento
+typedef struct elemento {
+   int valor;
+   struct elemento* anterior;
+   struct elemento* proximo;
+} Elemento;
+
+// Estrutura da lista
+typedef struct {
+   Elemento* inicio;
+   Elemento* fim; 
+} Lista;
+
+// Funções da biblioteca
+Lista* criar_lista();
+int lista_vazia(Lista* lista);
+int inserir_inicio(int valor, Lista* lista);
+int inserir_fim(int valor, Lista* lista);
+int remover_inicio(Lista* lista);
+int remover_fim(Lista* lista);
+void imprimir(Lista* lista);
+void imprimir_reverso(Lista* lista);
+void liberar_lista(Lista* lista);
+int tamanho(Lista* lista);
+
+#endif
